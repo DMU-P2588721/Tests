d28fd9ab041013a1a312f472e0b5b7b1bf9e7e88
DMU-P2588721 <72395474+DMU-P2588721@users.noreply.github.com>
2021-02-26 06:34:41 +0000

    members page

--- CW-2/JS/gallery.js
+++ CW-2/JS/gallery.js
@@ -1,20 +1,29 @@
-var slideIndex = [1,1];
+/*Variable with the starting slide numbers*/
+var slideIndex = [1,1,1,1,1,1,1,1];
 /* Class the members of each slideshow group with different CSS classes */
-var slideId = ["mySlides1", "mySlides2"]
+var slideId = ["mySlides1", "mySlides2", "mySlides3", "mySlides4", "mySlides5", "mySlides6", "mySlides7", "mySlides8"]
+
 showSlides(1, 0);
 showSlides(1, 1);
+showSlides(1, 2);
+showSlides(1, 3);
+showSlides(1, 4);
+showSlides(1, 5);
+showSlides(1, 6);
+showSlides(1, 7);
 
-function plusSlides(n, no) {
-  showSlides(slideIndex[no] += n, no);
+
+function plusSlides(change, gal_no) {   /*Input from html, the change in slide and the gallery that is being changed*/
+  showSlides(slideIndex[gal_no] += change, gal_no);   /*Runs a function to change the slide accordingly*/
 }
 
-function showSlides(n, no) {
+function showSlides(slide, gal_no) {
   var i;
-  var x = document.getElementsByClassName(slideId[no]);
-  if (n > x.length) {slideIndex[no] = 1}
-  if (n < 1) {slideIndex[no] = x.length}
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
+  var x = document.getElementsByClassName(slideId[gal_no]);
+  if (slide > x.length) {slideIndex[gal_no] = 1}    /*If the slide number is greater than the length of the gallery, sets the slide to be 1 (first image/video)*/
+  if (slide < 1) {slideIndex[gal_no] = x.length}    /*If the slide number is less than the length of the gallery, sets the slide to be last image/video)*/
+  for (i = 0; i < x.length; i++) {    /*For each number, if the number is less than the lenth of the gallery, add 1 to it*/
+    x[i].style.display = "none";    /*Hides the image/video*/
   }
-  x[slideIndex[no]-1].style.display = "block";
+  x[slideIndex[gal_no]-1].style.display = "block";    /*Shows the image/video*/
 }
